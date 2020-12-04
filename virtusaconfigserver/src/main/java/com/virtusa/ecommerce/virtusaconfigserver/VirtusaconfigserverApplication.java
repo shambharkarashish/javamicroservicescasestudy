aecd2c1804c636a5896f2a76e7be25632257e075
shamb <shamb@DESKTOP-I4CVCNF>
2020-12-04 17:30:06 +0000

    added virtusa config server

---
+++ virtusaconfigserver/src/main/java/com/virtusa/ecommerce/virtusaconfigserver/VirtusaconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.ecommerce.virtusaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtusaconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtusaconfigserverApplication.class, args);
+	}
+
+}
---
+++ virtusaconfigserver/src/test/java/com/virtusa/ecommerce/virtusaconfigserver/VirtusaconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.ecommerce.virtusaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtusaconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
